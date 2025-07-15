a00aa4d8eea6332bf6edf624f4be65c6e009d414
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-07-15 17:05:02 +0400

    style: luxera ai chat position

--- app/[locale]/luxera-ai/page.tsx
+++ app/[locale]/luxera-ai/page.tsx
@@ -18,7 +18,7 @@ const LuxeraAI = () => {
         <div className="flex flex-col gap-4 flex-1 overflow-y-auto">
           <Chat />
         </div>
-        <div className="fixed md:static bottom-0 left-0 right-0 px-4 py-2  md:p-0">
+        <div className="fixed md:static  bottom-6 left-0 right-0 px-4 py-2  md:p-0">
           <ChatInput />
         </div>
       </div>
