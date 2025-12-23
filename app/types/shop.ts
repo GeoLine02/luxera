c73ffbc591d7eadbef660259dc5cd207528c68ca
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-12-23 15:32:44 +0400

    feat: display shop location on shop details page

--- app/types/shop.ts
+++ app/types/shop.ts
@@ -5,8 +5,16 @@ export interface ShopRegisterType {
   repeatPassword: string;
 }
 
+export interface ShopCityType {
+  id: number;
+  city_name: string;
+}
+
 export interface ShopType {
   id: number;
   owner_id: number;
   shop_name: string;
+  city_id: number;
+  custom_city_name: string;
+  city: ShopCityType;
 }
