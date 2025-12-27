568e516a1c2fc7438458e8d1cade6c4c0eceb59c
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-12-27 10:14:39 +0400

    hotfix: add temporary image and remove w-min from subcategory card label

--- app/shared/categories/SubCategoryCard.tsx
+++ app/shared/categories/SubCategoryCard.tsx
@@ -1,5 +1,7 @@
 import { SubCategoryType } from "@/app/types/categories";
 import Link from "next/link";
+import GiftBoxImage from "@/public/giftbox.png";
+import Image from "next/image";
 
 const SubCategoryCard = ({ sub_category_name, id }: SubCategoryType) => {
   return (
@@ -7,14 +9,9 @@ const SubCategoryCard = ({ sub_category_name, id }: SubCategoryType) => {
       href={`/products?subcategory=${sub_category_name}-${id}`}
       className={`bg-light-pink rounded-md card px-2 flex items-center justify-around w-full  box-border lg:max-w-full h-[90px] cursor-pointer`}
     >
-      <h1 className="w-min font-medium">{sub_category_name}</h1>
+      <h1 className="font-medium">{sub_category_name}</h1>
 
-      {/* <Image
-        height={50}
-        width={50}
-        src={subCategoryImage}
-        alt={subCategoryName + "image"}
-      /> */}
+      <Image height={50} width={50} src={GiftBoxImage} alt={""} />
     </Link>
   );
 };
