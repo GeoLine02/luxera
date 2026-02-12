37fce9400bdd47acc20333d7d1552fd5c661340e
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2026-02-12 21:06:52 +0400

    chore: update varint id with product id

--- app/[locale]/luxera-ai/[id]/components/MessageCard.tsx
+++ app/[locale]/luxera-ai/[id]/components/MessageCard.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { MessageProductType } from "@/app/types/ai";
 import Button from "@/app/ui/Button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface MessageCardProps {
   content: string;
@@ -8,50 +11,52 @@ interface MessageCardProps {
 }
 
 const MessageCard = ({ content, products }: MessageCardProps) => {
+  const router = useRouter();
+
   return (
     <div className="text-white w-full lg:max-w-[50%]  bg-dark-gray p-4 rounded-xl">
       <p>{content}</p>
       <div className="mt-4 flex flex-col gap-2 ">
-        {products?.map(
-          (product) => (
-            console.log(product),
-            (
-              <div
-                className="flex gap-4 border p-2 rounded-xl border-medium-gray items-center"
-                key={product.link}
-              >
-                <Image
-                  className="rounded-xl max-w-[150px] object-cover aspect-square"
-                  width={130}
-                  height={130}
-                  src={product.imageUrl}
-                  alt=""
-                />
-                <div className="space-y-2 w-full">
-                  <h1 className="text-lg md:text-2xl font-bold max-w-[90%] whitespace-nowrap truncate">
-                    {product.variantName}
-                  </h1>
-                  <div className="flex gap-2 items-center">
-                    <span className="md:text-lg text-gray-400 line-through">
-                      {product.variantPrice?.toFixed(2)} Gel
-                    </span>
-                    <span className="bg-red-500 text-white text-sm font-semibold px-2 py-1 rounded-md">
-                      -{product.variantDiscount}%
-                    </span>
-                  </div>
-                  <Button
-                    className="py-1.5 md:py-3 font-medium sm:max-w-1/2"
-                    rounded="lg"
-                    title="Visit Product"
-                    type="button"
-                    bgcolor="lightPink"
-                    titleColor="black"
-                  />
-                </div>
+        {products?.map((product) => (
+          <div
+            className="flex gap-4 border p-2 rounded-xl border-medium-gray items-center"
+            key={product.link}
+          >
+            <Image
+              className="rounded-xl max-w-[150px] object-cover aspect-square"
+              width={130}
+              height={130}
+              src={product.imageUrl}
+              alt=""
+            />
+            <div className="space-y-2 w-full">
+              <h1 className="text-lg md:text-2xl font-bold max-w-[90%] whitespace-nowrap truncate">
+                {product.variantName}
+              </h1>
+              <div className="flex gap-2 items-center">
+                <span className="md:text-lg text-gray-400 line-through">
+                  {product.variantPrice?.toFixed(2)} Gel
+                </span>
+                <span className="bg-red-500 text-white text-sm font-semibold px-2 py-1 rounded-md">
+                  -{product.variantDiscount}%
+                </span>
               </div>
-            )
-          ),
-        )}
+              <Button
+                onClick={() =>
+                  router.push(
+                    `/product/${product.variantName}-${product.productId}`,
+                  )
+                }
+                className="py-1.5 md:py-3 font-medium sm:max-w-1/2"
+                rounded="lg"
+                title="Visit Product"
+                type="button"
+                bgcolor="lightPink"
+                titleColor="black"
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
